675b84ce49206a930d7de5fc293372ba6cb3b0c4
Hitoshi Okonogi <hitoshiokonogi@kink.com>
2014-08-26 16:43:45 -0700

    Add clustername

--- mongodb-mysite/attributes/configserver.rb
+++ mongodb-mysite/attributes/configserver.rb
@@ -1,1 +1,2 @@
 default['mongodb']['config']['port'] = 27019
+default[:mongodb][:cluster_name]   = "KL3"
---
+++ mongodb-mysite/attributes/shards.rb
@@ -0,0 +1,2 @@
+default[:mongodb][:cluster_name] = 'KL3'
+default[:mongodb][:shard_name]   = node[:nodeworks][:instance][:hostname]
