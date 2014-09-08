2b7f06cd763711ad1354e71d329650d3a8d45be2
Hitoshi Okonogi <hitoshiokonogi@kink.com>
2014-09-08 11:10:26 -0700

    Adding search log

--- mongodb-mysite/recipes/mongos.rb
+++ mongodb-mysite/recipes/mongos.rb
@@ -1,2 +1,13 @@
 include_recipe "mongodb::10gen_repo"
+
 include_recipe "mongodb::install"
+include_recipe "mongodb::mongo_gem"
+
+configsrvs = search(
+	:node,
+	"mongodb_cluster_name:#{node['mongodb']['cluster_name']} AND \
+	mongodb_is_configserver:true AND \
+	chef_environment:#{node.chef_environment}"
+)
+
+Chef.Log.error("configsrvs length: #{configsrvs.length}")
