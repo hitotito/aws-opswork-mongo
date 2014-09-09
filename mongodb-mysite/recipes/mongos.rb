a6153f4b0265e86d1cbb1636c6aeed56c08be546
Hitoshi Okonogi <hitoshiokonogi@kink.com>
2014-09-09 13:53:50 -0700

    Test

--- mongodb-mysite/recipes/mongos.rb
+++ mongodb-mysite/recipes/mongos.rb
@@ -1,7 +1,15 @@
 include_recipe "mongodb::10gen_repo"
 
-include_recipe "mongodb::install"
-include_recipe "mongodb::mongo_gem"
+node.set['mongodb']['is_mongos'] = true
+node.set['mongodb']['shard_name'] = node['mongodb']['shard_name']
+node.override['mongodb']['instance_name'] = 'mongos'
+
+include_recipe 'mongodb::install'
+include_recipe 'mongodb::mongo_gem'
+
+service node[:mongodb][:default_init_name] do
+	  action [:disable, :stop]
+end
 
 configsrvs = search(
 	:node,
@@ -18,3 +26,14 @@ test = search(
 )
 
 Chef::Log.error("test array : #{test.length}")
+
+
+mongodb_instance node['mongodb']['instance_name'] do
+	mongodb_type 'mongos'
+	port         node['mongodb']['config']['port']
+	logpath      node['mongodb']['config']['logpath']
+	dbpath       node['mongodb']['config']['dbpath']
+	configservers test
+	enable_rest  node['mongodb']['config']['rest']
+	smallfiles   node['mongodb']['config']['smallfiles']
+end
