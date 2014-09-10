ad6bed9796d64ba0ed52a05b2c042746a527f721
Hitoshi Okonogi <hitoshiokonogi@kink.com>
2014-09-09 17:28:27 -0700

    Test

--- mongodb-mysite/recipes/mongos.rb
+++ mongodb-mysite/recipes/mongos.rb
@@ -16,37 +16,6 @@ configsrvs = search(
 	"mongodb_cluster_name:#{node['mongodb']['cluster_name']}"
 )
 
-Chef::Log.error("configsrvs cluster_name : #{node['mongodb']['cluster_name']}")
-Chef::Log.error("configsrvs length: #{configsrvs.length}")
-
-
-test1 = search(
-	:node,
-	"mongodb_cluster_name:#{node['mongodb']['cluster_name']}"
-)
-Chef::Log.error("test array : #{test1.length}")
-test1.each{ |x|
-	Chef::Log.error("fqdn for test : #{x['fqdn']}")
-}
-
-test2  = search(
-	:node,
-	"chef_environment:#{node.chef_environment}"
-)
-Chef::Log.error("test array : #{test2.length}")
-test2.each{ |x|
-	Chef::Log.error("fqdn for test : #{x['fqdn']}")
-}
-
-test3  = search(
-	:node,
-	"mongodb_is_configserver:true"
-)
-Chef::Log.error("test array : #{test3.length}")
-test3.each{ |x|
-	Chef::Log.error("fqdn for test : #{x['fqdn']}")
-}
-
 # mongodb_instance node['mongodb']['instance_name'] do
 # 	mongodb_type 'mongos'
 # 	port         node['mongodb']['config']['port']
--- mongodb-mysite/recipes/test.rb
+++ mongodb-mysite/recipes/test.rb
@@ -2,27 +2,27 @@ test1 = search(
 	:node,
 	"mongodb_cluster_name:#{node['mongodb']['cluster_name']}"
 )
-Chef::Log.error("test array : #{test1.length}")
+Chef::Log.error("test1 array : #{test1.length}")
+Chef::Log.error("test1 culster_name : #{node['mongodb']['cluster_name']}")
 test1.each{ |x|
-	Chef::Log.error("fqdn for test : #{x['fqdn']}")
+	Chef::Log.error("test1: fqdn for test : #{x['fqdn']}")
 }
 
 test2  = search(
 	:node,
 	"chef_environment:#{node.chef_environment}"
 )
-Chef::Log.error("test array : #{test2.length}")
+Chef::Log.error("test2 array : #{test2.length}")
+Chef::Log.error("test2 env: #{node.chef_environment}")
 test2.each{ |x|
-	Chef::Log.error("fqdn for test : #{x['fqdn']}")
+	Chef::Log.error("test2: fqdn for test : #{x['fqdn']}")
 }
 
 test3  = search(
 	:node,
 	"mongodb_is_configserver:true"
 )
-Chef::Log.error("test array : #{test3.length}")
+Chef::Log.error("test3 array : #{test3.length}")
 test3.each{ |x|
-	Chef::Log.error("fqdn for test : #{x['fqdn']}")
+	Chef::Log.error("test3: fqdn for test : #{x['fqdn']}")
 }
-
-
