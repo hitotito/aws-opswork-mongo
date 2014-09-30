aaf016148719737628b7258d58d570e07ab8bb0f
Hitoshi Okonogi <hitotito@gmail.com>
2014-09-29 17:01:32 -0700

    Add iptables rule

---
+++ mongodb-mysite/recipes/iptables.rb
@@ -0,0 +1,9 @@
+script "iptables" do
+	interpreter "bash"
+	user "root"
+	cwd "/tmp"
+	code <<-EOH
+		iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 5000
+		iptables -t nat -I OUTPUT -p tcp -o lo --dport 80 -j REDIRECT --to-ports 5000
+	EOH
+end
