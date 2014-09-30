4d6c6ffeaba6d779d6d9b6b1304992aa688637ef
Hitoshi Okonogi <hitotito@gmail.com>
2014-09-29 17:20:53 -0700

    Save iptables rule

--- mongodb-mysite/recipes/iptables.rb
+++ mongodb-mysite/recipes/iptables.rb
@@ -5,5 +5,6 @@ script "iptables" do
 	code <<-EOH
 		iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 5000
 		iptables -t nat -I OUTPUT -p tcp -o lo --dport 80 -j REDIRECT --to-ports 5000
+		service iptables save
 	EOH
 end
