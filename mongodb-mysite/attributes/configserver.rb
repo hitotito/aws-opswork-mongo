52280215c6be1aa804941794059d735092e57324
Hitoshi Okonogi <hitotito@gmail.com>
2014-09-29 17:41:09 -0700

    Try this out

--- mongodb-mysite/attributes/configserver.rb
+++ mongodb-mysite/attributes/configserver.rb
@@ -1,3 +1,1 @@
-default['mongodb']['is_configserver'] = true
-default['mongodb']['config']['port']  = 27019
-default[:mongodb][:cluster_name]      = "KL3"
+default[:mongodb][:cluster_name]      = "STAN"
--- mongodb-mysite/recipes/configserver.rb
+++ mongodb-mysite/recipes/configserver.rb
@@ -1,2 +1,5 @@
 include_recipe "mongodb::10gen_repo"
 include_recipe "mongodb::configserver"
+
+default['mongodb']['is_configserver'] = true
+default['mongodb']['config']['port']  = 27019
