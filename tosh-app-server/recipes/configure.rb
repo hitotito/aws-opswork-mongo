1fbd2e610bd40506d2d34660430f948d81fb0695
Hitoshi Okonogi <hitotito@gmail.com>
2014-10-01 14:09:08 -0700

    1

---
+++ tosh-app-server/recipes/configure.rb
@@ -0,0 +1,27 @@
+script "iptables" do
+	interpreter "bash"
+	user "root"
+	cwd "/tmp"
+	code <<-EOH
+		iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 5000
+		iptables -t nat -I OUTPUT -p tcp -o lo --dport 80 -j REDIRECT --to-ports 5000
+		service iptables save
+	EOH
+end
+
+script "mongos" do
+	interpreter "bash"
+	user "root"
+	cwd "/tmp"
+	code <<-EOH
+		mongos --configdb configdb2:27019 --fork --logpath /var/log/mongodb/mongod.log
+	EOH
+end
+
+file "/var/log/node/stan.log" do
+	mode  "0644"
+	owner "root"
+	group "root"
+	action :create
+end
+
--- tosh-app-server/recipes/default.rb
+++ tosh-app-server/recipes/default.rb
@@ -2,31 +2,3 @@ include_recipe "mongodb::10gen_repo"
 include_recipe "mongodb::install"
 
 package "tmux"
-
-script "iptables" do
-	interpreter "bash"
-	user "root"
-	cwd "/tmp"
-	code <<-EOH
-		iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 5000
-		iptables -t nat -I OUTPUT -p tcp -o lo --dport 80 -j REDIRECT --to-ports 5000
-		service iptables save
-	EOH
-end
-
-script "iptables" do
-	interpreter "bash"
-	user "root"
-	cwd "/tmp"
-	code <<-EOH
-		mongos --configdb configdb2:27019 --fork --logpath /var/log/mongodb/mongod.log
-	EOH
-end
-
-file "/var/log/node/stan.log" do
-	mode  "0644"
-	owner "root"
-	group "root"
-	action :create
-end
-
