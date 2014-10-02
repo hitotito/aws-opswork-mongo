e971d1dced23d1fdb351711a9c80ad85598c25fe
Hitoshi Okonogi <hitotito@gmail.com>
2014-10-02 15:04:17 -0700

    Fix

--- tosh-app-server/recipes/configure.rb
+++ tosh-app-server/recipes/configure.rb
@@ -1,15 +1,3 @@
-iptables_ng_rule '99-redirect' do
-	chain 'PREROUTING'
-	table 'nat'
-	rule '-p tcp --dport 80 -j REDIRECT --to-ports 5000'
-end
-
-iptables_ng_rule '100-loopbackredirect' do
-	chain 'OUTPUT'
-	table 'nat'
-	rule '-p tcp -o lo --dport 80 -j REDIRECT --to-ports 5000'
-end
-
 script "mongos" do
 	interpreter "bash"
 	user "root"
