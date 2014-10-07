ef9885ff8ab604bd28e13e89bc7a31bdd51be6f7
Hitoshi Okonogi <hitotito@gmail.com>
2014-10-07 14:17:56 -0700

    fix replica set

--- tosh-mongodb/attributes/default.rb
+++ tosh-mongodb/attributes/default.rb
@@ -1,4 +1,5 @@
 node.default['mongodb']['config']['port']  = 27017
-node.default['mongodb']['cluster_name'] = 'stan-stage1'
+node.default['mongodb']['replicaset_name'] = 'rs_stan_stage1'
+node.default['mongodb']['cluster_name']	   = 'rs_stan_stage1'
 
-
+node.default[:mongodb][:config][:replSet] = 'rs_stan_stage1'
