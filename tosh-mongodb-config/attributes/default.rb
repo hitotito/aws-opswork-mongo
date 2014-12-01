9740c117e10f5f277bf413c3f277d97da334f3b6
Hitoshi Okonogi <hitotito@gmail.com>
2014-12-01 11:30:39 -0800

    Change to KL3 cluster

--- tosh-mongodb-config/attributes/default.rb
+++ tosh-mongodb-config/attributes/default.rb
@@ -1,3 +1,3 @@
-default[:mongodb][:cluster_name]      = "STAN"
+default[:mongodb][:cluster_name]      = "KL3"
 default['mongodb']['is_configserver'] = true
 default['mongodb']['config']['port']  = 27019
