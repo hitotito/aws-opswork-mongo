310cf678220c9227ba47aaeca6b70bfaee336ea8
Hitoshi Okonogi <hitotito@gmail.com>
2014-12-02 10:44:47 -0800

    change port to 8000

--- tosh-app-server/recipes/default.rb
+++ tosh-app-server/recipes/default.rb
@@ -6,13 +6,11 @@ package "tmux"
 iptables_ng_rule '99-redirect' do
 	chain 'PREROUTING'
 	table 'nat'
-	rule '-p tcp --dport 80 -j REDIRECT --to-ports 5000'
+	rule '-p tcp --dport 80 -j REDIRECT --to-ports 8000'
 end
 
 iptables_ng_rule '100-loopbackredirect' do
 	chain 'OUTPUT'
 	table 'nat'
-	rule '-p tcp -o lo --dport 80 -j REDIRECT --to-ports 5000'
+	rule '-p tcp -o lo --dport 80 -j REDIRECT --to-ports 8000'
 end
-
-
