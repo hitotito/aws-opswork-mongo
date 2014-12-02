4acf2caa97b913668ede55a9e5e986421d27ac06
Hitoshi Okonogi <hitotito@gmail.com>
2014-12-01 17:11:52 -0800

    change default config path

--- tosh-mongodb/attributes/default.rb
+++ tosh-mongodb/attributes/default.rb
@@ -1,3 +1,4 @@
+node.default['mongodb']['dbconfig_file']   = '/etc/mongodb.conf'
 node.default['mongodb']['config']['port']  = 27017
 node.default['mongodb']['replicaset_name'] = 'rs_stan_stage1'
 node.default['mongodb']['cluster_name']	   = 'rs_stan_stage1'
