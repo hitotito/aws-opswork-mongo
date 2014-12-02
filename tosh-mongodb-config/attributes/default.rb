da7aa4cd609b9a7c815a47fb66797221a315144b
Hitoshi Okonogi <hitotito@gmail.com>
2014-12-01 18:17:37 -0800

    try using existing mongod.conf file

--- tosh-mongodb-config/attributes/default.rb
+++ tosh-mongodb-config/attributes/default.rb
@@ -1,3 +1,4 @@
+default[:mongodb][:dbconfig_file] = '/etc/mongod.conf'
 default[:mongodb][:cluster_name]      = "KL3"
 default['mongodb']['is_configserver'] = true
 default['mongodb']['config']['port']  = 27019
